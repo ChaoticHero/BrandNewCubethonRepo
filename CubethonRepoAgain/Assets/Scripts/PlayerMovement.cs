b0d2768ab0f146fa2907daf58add39b66fde39c8
ChaoticHero <123209040+ChaoticHero@users.noreply.github.com>
2025-01-16 20:11:02 -0700

    Game

---
+++ CubethonRepoAgain/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // This is a reference to the Rigidbody component called "rb"
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // We marked this as "FixedUpdate" because are using it to mess with physics
+    private void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime); // Add a force of 2000 on the z-axis, Time.deltaTime - used that works same for each system mobile or pc with different perfomance
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if(rb.position.y < -1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
